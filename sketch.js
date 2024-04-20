f3e7aec43f0bee48f3ff03de416a9c56ddf5f8b1
Malina <borosmalina@gmail.com>
2024-04-20 15:59:02 +0300

    File changes

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let wordNouns; 
+let wordVerbs;
+let wordPronoun;
+let wordAdjective;
+let wordPreposition;
+let wordInterjection;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
